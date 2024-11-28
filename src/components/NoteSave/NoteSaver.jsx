45ba8774ee67e37342241171e889c67177cb6661
¨martinvruiz¨ <martinvruiz10@gmail.com>
2024-11-28 13:36:29 -0300

    added toastify and implemented color for the notes

--- src/components/NoteSave/NoteSaver.jsx
+++ src/components/NoteSave/NoteSaver.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const NoteSaver = ()=>{
 
@@ -11,30 +13,45 @@ useEffect(()=>{
 },[])
 
 const handleNote = (e)=>{
-setNote(e.target.value)
+        setNote(e.target.value)
 }
 
 const handleSubmit = (e)=>{
     e.preventDefault()
 
+    const noteColor = document.getElementById("color-note").value
+
+
     if(note === ""){
+        toast.error("Ingrese su nota!")
         console.log("No hay datos")
         return
     }else{
-        const newNote = {id: Date.now(), value: note}
+        const newNote = {  
+            id: Date.now(),
+            value: note, 
+            color: noteColor
+        }
         const newNotes = [...notes, newNote]
         setNotes(newNotes)
         localStorage.setItem("savedNotes", JSON.stringify(newNotes))
-        setNote('')
+        toast("Nota guardada!")
 }}
 
 
 
 
-    return <>
+    return <div>
     
         <form action="note" className=" my-2 p-2 flex flex-col gap-3 w-full items-center">
-            <input type="text" placeholder="Ingrese su nota!" onChange={handleNote} maxLength="50" className="border border-blue-600 focus:border-4 focus:outline-none text-center w-2/5 h-1/5 rounded-xl min-h-full"/>
+            <select name="color-note" id="color-note" className="p-2 border-2 border-blue-600 rounded-lg">
+                <option value="border-blue-600">Azul</option>
+                <option value="border-violet-600">Violeta</option>
+                <option value="border-pink-600">Rosa</option>
+                <option value="border-red-600">Rojo</option>
+                <option value="border-green-600">Verde</option>
+            </select>
+            <input type="text" placeholder="Ingrese su nota!" onChange={handleNote} maxLength="50" className="border border-blue-600 focus:border-4 focus:outline-none text-center w-4/5 rounded-xl min-h-24"/>
             <button type="submit" onClick={handleSubmit} className="relative p-3 w-1/5 block text-white bg-blue-600 rounded-lg hover:bg-blue-800">Guardar nota</button>
         </form>
         <div className="mt-4 p-4 w-full">
@@ -46,13 +63,14 @@ const handleSubmit = (e)=>{
             notes.slice(-3).map((note) => (
                 <li
                     key={note.id}
-                    className="p-4 m-2 border-2 border-blue-600 text-center rounded-xl"
+                    className={`p-4 m-2 border-4 ${note.color} text-center rounded-xl`} 
                 >
                     <h4 className="text-xs">Nota N: {note.id}</h4>
                     <p className="text-xl">{note.value}</p>
                 </li>
             ))}
         </ul>
+        <ToastContainer autoClose={1000}/>
         </div>
-    </>
+    </div>
 }
--- src/components/SavedNotes/SavedNotes.jsx
+++ src/components/SavedNotes/SavedNotes.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react"
+import { ToastContainer, toast } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 
 export const SavedNotes = ()=>{
 
@@ -16,20 +18,56 @@ export const SavedNotes = ()=>{
         localStorage.setItem("savedNotes", JSON.stringify(updateNotes))
     }
 
+    const btnDelete = (id)=>{
+        toast.warning(
+            <div className='text-black h-full'>
+                <span>
+                    <p>
+                        ¿Desea eliminar el registro?
+                    </p>
+                    <p>{id}</p>
+                </span>
+                <div>
+                    <button
+                    onClick={() => {
+                    handleDelete(id);
+                    toast.dismiss();
+                    }}
+                    className=' bg-black text-white rounded-md my-2 mx-6 px-2'
+                    >
+                        Sí
+                    </button>
+                    <button
+                    onClick={() => toast.dismiss()}
+                    className=' bg-black text-white rounded-md my-2 mx-6 px-2'
+                    >
+                        No
+                    </button>
+                </div>
+            </div>,
+            {
+                closeButton: false,
+            }
+        );
+    }; 
+
     return <>
         <div className="flex flex-col items-center w-full my-2">
             <h1 className="my-2 p-2 lg:text-2xl font-semibold">Tus notas</h1>
             <p className="text-center">Aca podes encontrar tus notas guardadas!</p>
-            <ul className="flex flex-wrap justify-center">
+            <ul className=" my-2 py-2 flex flex-wrap justify-center">
             {notes.map((note)=>(
-                <li key={note.id} className="p-4 m-2 border-2 border-blue-600 text-center rounded-xl">
+                <li key={note.id} className={`p-4 m-2 border-4 ${note.color} text-center rounded-xl`}>
                     <h4 className="text-xs">Nota N: {note.id}</h4>
                     <p className="text-xl">{note.value}</p>
-                    <button onClick={()=>handleDelete(note.id)} className="text-xs text-red-600 p-1 m-1 border border-red-600 rounded-md">Eliminar</button>
+                    <button onClick={()=>btnDelete(note.id)} className="text-xs text-red-600 p-1 m-1 border border-red-600 rounded-md">Eliminar</button>
+                    <ToastContainer autoClose={4000}/>
                 </li>
+                
             ))
                 }
             </ul>
+            
         </div>
     </>
 }
